88ee59d31eeee31af950afeb1cbf1e692e7709a0
nicoleshin <shnnicole12321@yahoo.com>
2016-12-24 16:25:48 -0500

    More refactoring
    Also reimplemement erasing when you right click because I accidentally got rid
    of that in the last commit oops.

--- Main.java
+++ Main.java
@@ -22,24 +22,29 @@ public class Main extends Application {
         Group root = new Group();                                         //makes a group thing
         Canvas canvas = new Canvas(WIDTH,HEIGHT);                         //there is now a canvas of size WIDTH HEIGHT 
         GraphicsContext gc = canvas.getGraphicsContext2D();               //canvas has graphical abilities and such
-        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED,
-                new EventHandler<MouseEvent>() {
-                    @Override
+
+        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, new EventHandler<MouseEvent>() {
+            @Override
             public void handle(MouseEvent e) {
                 gc.setFill(Paint.valueOf("RED"));
-                if (e.isPrimaryButtonDown() || e.isSecondaryButtonDown()) {
+                if (e.isPrimaryButtonDown()) {
                     gc.fillRect(e.getX() - 2, e.getY() - 2, 4, 4);     //Draw a red rectangle at place of left-mouse-drag
                 }
+                if (e.isSecondaryButtonDown()){
+                    gc.clearRect(e.getX() - 2, e.getY() - 2, 4, 4);    //Erase a rectangle at place of right-mouse-drag
+                }
             }
-            });
+        });
+
         canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent t) {
-                if ((t.getClickCount() >1) && (t.getButton() == MouseButton.SECONDARY)) {
+                if (t.getClickCount() >1 && t.isSecondaryButtonDown()) {
                     reset(canvas, Color.WHITE);                        //Method "reset" at place of double-right-click (see "reset" below)
                 }
             }
         });
+
         root.getChildren().add(canvas);                                   //the group "root" now has the previously created canvas in it
         stage.setScene(new Scene(root));                                  //the stage's scene is now the group "root" (consisting of "canvas")
         stage.show();                                                     //the stage is now shown
